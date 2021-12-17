7d41d5eb7ace93180ab032fa0da3ea5c49dfa83f
Роман Кодочигов <kodochigovra@mail.ru>
2021-12-17 18:31:54 +0300

    ready lesson_4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
+
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
