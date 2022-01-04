b2be9cdb63e9772174c7d7887529bcb3f054a853
Роман Кодочигов <kodochigovra@mail.ru>
2022-01-04 16:11:20 +0300

    ready lesson_8

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.utils.decorators import method_decorator
 from authapp.models import ShopUser
 from authapp.forms import ShopUserRegisterForm
 from mainapp.models import Product, ProductCategory
-from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm
+from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm, ProductEditForm
 
+
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
@@ -83,7 +95,6 @@ def user_delete(request, pk):
     return render(request, 'adminapp/user_delete.html', content)
 
 
-
 @user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = 'админка/категории'
@@ -96,6 +107,13 @@ def categories(request):
     }
 
     return render(request, 'adminapp/categories.html', content)
+
+
+class ProductCategoryCreateView(CreateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -117,6 +135,18 @@ def category_create(request):
 
     return render(request, 'adminapp/category_update.html', content)
 
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+
+        return context
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -140,6 +170,18 @@ def category_update(request, pk):
     }
 
     return render(request, 'adminapp/category_update.html', content)
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'adminapp/category_to_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+
+        return HttpResponseRedirect(self.get_success_url())
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -179,19 +221,81 @@ def products(request, pk):
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
-    pass
+    title = 'продукт/создание'
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    if request.method == 'POST':
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product_form.save()
+            return HttpResponseRedirect(reverse('admin:products', args=[pk]))
+    else:
+        product_form = ProductEditForm(initial={'category': category})
+
+    content = {
+        'title': title,
+        'update_form': product_form,
+        'category': category,
+    }
+
+    return render(request, 'adminapp/product_update.html', content)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'adminapp/product_read.html'
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
-    pass
+    title = 'продукт/подробнее'
+    product = get_object_or_404(Product, pk=pk)
+    content = {
+        'title': title,
+        'object': product,
+    }
+
+    return render(request, 'adminapp/product_read.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
-    pass
+    title = 'продукт/редактирование'
+
+    edit_product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('adminapp:product_update', args=[edit_product.pk]))
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+
+    content = {
+        'title': title,
+        'update_form': edit_form,
+        'category': edit_product.category,
+    }
+
+    return render(request, 'adminapp/product_update.html', content)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
-    pass
+    title = 'продукт/удаление'
+
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        # product.is_active = False
+        product.delete()
+        return HttpResponseRedirect(reverse('admin:products', args=[product.category.pk]))
+
+    content = {
+        'title': title,
+        'product_to_delete': product,
+    }
+
+    return render(request, 'adminapp/product_delete.html', content)
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
 from basketapp.models import Basket
 import random
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 LINKS_MENU = [
     {'href': 'main', 'name': 'домой'},
@@ -41,28 +42,36 @@ def main(request):
     return render(request, 'mainapp/index.html', context=content)
 
 
-def products(request, pk=None):
-    print(pk)
-
+def products(request, pk=None, page=1):
     title = 'продукты'
     links_menu_category = ProductCategory.objects.all()
-
     basket = get_basket(request.user)
 
-    if pk != None:
+    if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by('price')
-            category = {'name': 'все'}
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products = Product.objects.filter(category__pk=pk).order_by('price')
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
 
         content = {
             'title': title,
             'links_menu': LINKS_MENU,
             'links_menu_category': links_menu_category,
             'category': category,
-            'products': products,
+            'products': products_paginator,
             'basket': basket,
         }
 
